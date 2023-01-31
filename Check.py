7acf15ae9a9ab3527c46299943121e4dd6b8a062
Nonthawt20 <Nonthawt20@gmail.com>
2023-01-31 22:38:31 +0700

    fix มันสลับกัน

--- Check.py
+++ Check.py
@@ -14,9 +14,9 @@ for i in range(0,n):
 		print("3")
 	if((X[i].count('B')==1) and (X[i].count('C')==1) ):
 		print("4")
-	if((X[i].count('A')==1) and (X[i].count('G')==1) and (X[i].count('F')==1) and (X[i].count('I')==1) ):
-		print("5")
 	if((X[i].count('A')==1) and (X[i].count('G')==1) and (X[i].count('F')==1) and (X[i].count('J')==1) ):
+		print("5")
+	if((X[i].count('A')==1) and (X[i].count('G')==1) and (X[i].count('F')==1) and (X[i].count('I')==1) ):
 		print("6")
 	if((X[i].count('A')==1) and (X[i].count('H')==1) and (X[i].count('N')==1) ):
 		print("7")
